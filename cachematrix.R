780031dac11970d288b01be82038324606b86a1b
placeacall <speakto_obayi@yahoo.com>
2014-05-23 18:35:59 +0100

    Update to the cacheMatrix.R with comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,26 @@
 ## functions do
 
 ## Write a short comment describing this function
+## You can use the code block to test the function
+
+# > a <- makeCacheMatrix()
+# > a$set(matrix(c(1, 0, 5, 2, 1, 6, 3, 4, 0),3,3) )
+# > a$get()
+# [,1] [,2] [,3]
+# [1,]    1    2    3
+# [2,]    0    1    4
+# [3,]    5    6    0
+# > cacheSolve(a)
+# [,1] [,2] [,3]
+# [1,]  -24   18    5
+# [2,]   20  -15   -4
+# [3,]   -5    4    1
+# > cacheSolve(a)
+# getting cached data
+# [,1] [,2] [,3]
+# [1,]  -24   18    5
+# [2,]   20  -15   -4
+# [3,]   -5    4    1
 
 makeCacheMatrix <- function(x = matrix()) 
 {
@@ -12,9 +32,9 @@ makeCacheMatrix <- function(x = matrix())
     m <<- NULL
   }
   
-  get <- function() x
+  get <- function() x ##Single line function that returns the value already set using object$set(...)
   
-  setInverse <- function(solve) m <<- solve
+  setInverse <- function(solve) m <<- solve 
   
   getInverse <- function() m
   
@@ -29,16 +49,18 @@ cacheSolve <- function(x, ...)
   ## Return a matrix that is the inverse of 'x'
   m <- x$getInverse()
   
-  if(!is.null(m)) {
+  if(!is.null(m)) ##Check if the inverse of the object has been cached/solved
+  {
     message("getting cached data")
     return(m)
   }
   
+  ##If null then get the already set value of the matrix
   data <- x$get()
   
-  m <- solve(data, ...)
+  m <- solve(data, ...)  ##Solve for the inverse of the matrix 
   
-  x$setInverse(m)
+  x$setInverse(m)  ##Set the inverse of the matrix
   
   return(m)
   
